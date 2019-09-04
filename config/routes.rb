ac749a3da7685a13326c049795486f0cef5c38ba
masa720 <gtrist_gtdx@yahoo.co.jp>
2019-09-04 22:02:14 +0900

    テーブル作成&ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :customers
   resources :negotiations do
     resources :continuations
+    resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
