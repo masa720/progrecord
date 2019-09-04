0c89173889db536c47bbfd055ec5d1411bc44128
masa720 <gtrist_gtdx@yahoo.co.jp>
2019-09-04 15:05:29 +0900

    コントローラ設定

--- app/controllers/negotiations_controller.rb
+++ app/controllers/negotiations_controller.rb
@@ -1,4 +1,23 @@
 class NegotiationsController < ApplicationController
   def index
   end
+
+  def new
+    @negotiation = Negotiation.new
+  end
+
+  def create
+    # binding.pry
+    @negotiation = Negotiation.new(new_params)
+    if @negotiation.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def new_params
+    params.require(:negotiation).permit(:customer_id, :year, :month, :day, :title, :body, :content, :importance, :department, :way, :next_nego, :next_year, :next_month, :next_day).merge(user_id: current_user.id)
+  end
 end
