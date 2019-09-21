fd204b575f79d588f3f5fee643bce151d5d17741
masa720 <gtrist_gtdx@yahoo.co.jp>
2019-09-21 16:36:19 +0900

    コメントアウト削除

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -18,24 +18,3 @@ $(function() {
     $(".main-nego__detail__box__comment-box").toggle();
   });
 });
-
-// $(function(){
-//   $(".comment-modal").click(function () {
-//     // ナビが表示される
-//     var clickPanel = $(".main-nego__detail__box__comment");
-//     clickPanel.toggle();
-//     $(".main-nego__detail__box__comment:visible")
-//     $(".main-nego__detail__box__comment:hidden")
-//     return false;
-//   });
-
-//   $(document).on('click', function () {
-//     if ($('.main-nego__detail__box__comment').is(':visible')) {
-//         // ナビが表示されていたらcloseを実行
-//         $('.main-nego__detail__box__comment').trigger('click');
-//     } else {
-//         // ナビが非表示の場合は起動しない
-//         event.stopPropagation();
-//     }
-//   });
-// });
