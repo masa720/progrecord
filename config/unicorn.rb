ef0a26631800d14da9b511f41b4b9e22eaccb1e6
masa720 <gtrist_gtdx@yahoo.co.jp>
2019-09-25 23:49:49 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,45 +1,37 @@
-app_path = File.expand_path('../../', __FILE__)
+#  unicron.rb
+  # set lets
+  $worker  = 2
+  $timeout = 30
+  $app_dir = "/home/ec2-user/var/www/" #アプリの場所
+  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+  $std_log = File.expand_path 'log/unicorn.log', $app_dir
 
-worker_processes 1
+  # set config
+  worker_processes  $worker
+  working_directory $app_dir
+  stderr_path $std_log
+  stdout_path $std_log
+  timeout $timeout
+  listen  $listen
+  pid $pid
 
-working_directory app_path
+  # loading booster
+  preload_app true
 
-pid "#{app_path}/tmp/pids/unicorn.pid"
-
-listen 3000
-
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-
-stdout_path "#{app_path}/log/unicorn.stdout.log"
-
-timeout 60
-
-preload_app true
-GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
-
-check_client_connection false
-
-run_once = true
-
-before_fork do |server, worker|
-  defined?(ActiveRecord::Base) &&
-    ActiveRecord::Base.connection.disconnect!
-
-  if run_once
-    run_once = false # prevent from firing again
-  end
-
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid) && server.pid != old_pid
-    begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH => e
-      logger.error e
+  # before starting processes
+  before_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+    old_pid = "#{server.config[:pid]}.oldbin"
+    if old_pid != server.pid
+      begin
+        Process.kill "QUIT", File.read(old_pid).to_i
+      rescue Errno::ENOENT, Errno::ESRCH
+      end
     end
   end
-end
 
-after_fork do |_server, _worker|
-  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
-end
+  # after finishing processes
+  after_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  end
