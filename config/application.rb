233095682c46468c4877426b923590bc974a6ce9
masa720 <gtrist_gtdx@yahoo.co.jp>
2019-09-26 23:16:50 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 module Progrecord
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    # config.load_defaults 5.2
     config.generators do |g|
       g.javascripts false
       g.helper false
