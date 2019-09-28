7842ee9f46ca1d62e3c628463d42b2a5fd58600d
masa720 <gtrist_gtdx@yahoo.co.jp>
2019-09-28 15:52:34 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'devise'
+# require 'devise'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 module Progrecord
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 5.2
+    config.load_defaults 5.2
     config.generators do |g|
       g.javascripts false
       g.helper false
