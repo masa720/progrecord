f626a7bcf5e8c5cd22a6f5aa31388a33dbf30172
masa720 <gtrist_gtdx@yahoo.co.jp>
2019-10-01 21:33:58 +0900

    header修正

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -30,8 +30,6 @@ module NotificationsHelper
   end
 
   def unchecked_notifications
-    if user_signed_in?
-      @notifications=current_user.passive_notifications.where(checked: false)
-    end
+    @notifications = current_user.passive_notifications.where(checked: false)
   end
 end
